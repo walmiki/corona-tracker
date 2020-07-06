b1c285d71a2dc64e2756dcd91c20d3069b4a7660
Sumit Walmiki <walmiki240@gmail.com>
2020-07-06 23:03:31 +0530

    Updates

---
+++ precache-manifest.c49b4dd101874a093cbc289f60754236.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7abeccc16ae14b3d09afe622d62ad629",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "2cb7f4d5d423fc472271",
+    "url": "/corona-tracker/static/css/main.084909cc.chunk.css"
+  },
+  {
+    "revision": "3637ee02d5725e30a34c",
+    "url": "/corona-tracker/static/js/2.d16339f0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.d16339f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cb7f4d5d423fc472271",
+    "url": "/corona-tracker/static/js/main.20fed95b.chunk.js"
+  },
+  {
+    "revision": "0dc2ef2a5542ad2ab0d0",
+    "url": "/corona-tracker/static/js/runtime-main.be612a41.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
